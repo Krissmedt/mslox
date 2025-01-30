6b2a540889a31f8baaf173cb4f1db8fdf9dc4eb0
krissmedt <kris.smedt@lorezen.com>
2025-01-30 21:47:31 +0000

    Token class

---
+++ mslox/Model/Token.cs
@@ -0,0 +1,11 @@
+﻿namespace mslox;
+
+public class Token
+{
+    private TokenType type { get; init; }
+    private String lexeme { get; init; }
+    private Object literal { get; init; }
+    private Int64 line { get; init; }
+    
+    public new String ToString() => $"{type} {lexeme} {literal}";
+}
