a24af5c7e6f50951f62484ea44120c6e16dc21d5
krissmedt <kris.smedt@lorezen.com>
2025-01-31 19:43:10 +0000

    Single-character lexemes

--- mslox/Service/Scanner.cs
+++ mslox/Service/Scanner.cs
@@ -2,12 +2,12 @@
 
 public class Scanner
 {
-    private String Source { get; init; }
+    public required String Source { get; init; }
     private List<Token> Tokens { get; } = new();
 
-    private Int64 _start = 0;
-    private Int64 _current = 0;
-    private Int64 _line = 0;
+    private Int32 _start = 0;
+    private Int32 _current = 0;
+    private Int32 _line = 0;
     
     public List<Token> Scan()
     {
@@ -29,11 +29,45 @@ public class Scanner
 
     private void ScanToken()
     {
-        throw new NotImplementedException();
+        char c = Advance();
+        switch (c) {
+            case '(': AddToken(TokenType.LeftParen); break;
+            case ')': AddToken(TokenType.RightParen); break;
+            case '{': AddToken(TokenType.LeftBrace); break;
+            case '}': AddToken(TokenType.RightBrace); break;
+            case ',': AddToken(TokenType.Comma); break;
+            case '.': AddToken(TokenType.Dot); break;
+            case '-': AddToken(TokenType.Minus); break;
+            case '+': AddToken(TokenType.Plus); break;
+            case ';': AddToken(TokenType.Semicolon); break;
+            case '*': AddToken(TokenType.Star); break; 
+        }
     }
 
     private Boolean IsAtEnd()
     {
         return _current >= Source.Length;
+    }
+
+    private Char Advance()
+    {
+        return Source.ToCharArray()[_current++];
+    }
+
+    private void AddToken(TokenType type)
+    {
+        AddToken(type, null);
+    }
+
+    private void AddToken(TokenType type, Object value)
+    {
+        var text = Source.Substring(_start, _current);
+        Tokens.Add(new Token
+        {
+            Type = type,
+            Lexeme = text,
+            Line = _line,
+            Literal = value
+        });
     }
 }
